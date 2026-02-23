33d187fc7bb66b720c097ad88ae5e278c64de85a
ilikefrogs101 <87593516+ilikefrogs101@users.noreply.github.com>
2026-02-23 16:53:46 +1100

    fix: increase buffer size

--- src/Controller/Program.cs
+++ src/Controller/Program.cs
@@ -5,15 +5,21 @@ using ilikefrogs101.Logging;
 
 namespace Lyrbd.Controller;
 public static class Program {
+    public const string SOCKET_PATH = "/tmp/lyrbd.sock";
     public static void Main() {
         Log.OnResponse += Console.WriteLine;
 
-        IpcClient client = new IpcClient();
-        client.Connect("/tmp/lyrbd.sock");
-        
-        string rawCommand = Environment.CommandLine.Split(' ', 2)[1];
-        client.Send(rawCommand);
+        if (File.Exists(SOCKET_PATH)) {
+            IpcClient client = new IpcClient();
+            client.Connect(SOCKET_PATH);
+            
+            string rawCommand = Environment.CommandLine.Split(' ', 2)[1];
+            client.Send(rawCommand);
 
-        Log.OutputResponse(client.Receive(16384));
+            Log.OutputResponse(client.Receive(4194304));
+        }
+        else {
+            Log.OutputResponse("lyrbd not running");
+        }
     }
 }
